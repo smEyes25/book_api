fbcd8c3361259f4380d093609e975105b976c176
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-14 08:49:36 +0700

    tét

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: '*',
     allowedHeaders: [
       'Content-Type',
       'Authorization',
       'Access-Control-Allow-Methods',
       'Access-Control-Request-Headers',
     ],
-    credentials: true,
-    preflightContinue: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   await app.listen(Constants.SERVER_PORT);
 }
